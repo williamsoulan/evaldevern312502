5c14ed5ae000983cfc515236b15e7841852a2932
williamsoulan <williamsoulan.form@gmail.com>
2025-06-20 14:17:52 +0200

    modif ViewConnexion.php

---
+++ App/View/ViewConnexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/assets/styles/main.css">
+    <script src="./public/assests/scripts/main.js"async></script>
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Se connecter</h1>
+    <form action="" method="post">
+        <label for="email">Veuillez saisir votre adresse email : </label>
+        <input type="email" name="email">
+        <label for="password">Veuillez saisir votre mot de passe : </label>
+        <input type="password" name="password">
+        <input type="submit" name="submit" value="connexion">
+    </form>
+</body>
+</html>
